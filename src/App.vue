c76e26692d45bdf720b3c42197ad6d94af46fde5
1440407551 <1440407551@qq.com>
2019-11-25 11:19:18 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <template>
-  <div>
-    <Header></Header>
-    <List></List>
-    <Footer></Footer>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header></Header>
+      <List></List>
+      <Footer></Footer>
+    </div>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import List from './components/List.vue'
-import Footer from './components/Footer.vue'
+  import Header from "./components/Header.vue";
+  import List from "./components/List.vue";
+  import Footer from "./components/Footer.vue";
 
-export default {
-  components: {
-    Header,
-    List,
-    Footer
-  }
+  export default {
+    components: {
+      Header,
+      List,
+      Footer
+    }
 };
 </script>
-<style>
-
+<style scoped>
+  /*app*/
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
  */
 import Vue from 'vue'
 import App from './App.vue'
+import './base.css'
+
 /* 只对当前文件有效 */
 /* eslint-disable no-new */
 new Vue({
